6eba19cfb92a28b3750585dcd5f0ac63b0e1c6be
iliyazz <iliyaz.softuni@gmail.com>
2023-02-19 22:45:49 +0200

    Create P02.VillainNames

---
+++ P04-excersise/P02.VillainNames/Config.cs
@@ -0,0 +1,8 @@
+﻿namespace P02Villain_Names
+{
+
+    internal class Config
+    {
+        public const string ConnectionString = @"Server=DESKTOP\SQLEXPRESS;Database=MinionsDB;trusted_connection=true;encrypt=false;";
+    }
+}
---
+++ P04-excersise/P02.VillainNames/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace P02Villain_Names
+{
+    using System.Text;
+    using Microsoft.Data.SqlClient;
+
+    public class StartUp
+    {
+
+        static async Task Main(string[] args)
+        {
+            await using SqlConnection sqlConnection = new SqlConnection(Config.ConnectionString);
+            await sqlConnection.OpenAsync();
+
+
+            string result = await GetAllVillainsAndCountOfTheirMinionsAsync(sqlConnection);
+            Console.WriteLine(result);
+        }
+        static async Task<string> GetAllVillainsAndCountOfTheirMinionsAsync(SqlConnection sqlConnection)//P02Villain Names
+        {
+            StringBuilder sb = new StringBuilder();
+            SqlCommand sqlCommand = new SqlCommand(SqlQueries.GetAllVillainsAndCountOfTheirMinions, sqlConnection);
+            SqlDataReader reader = await sqlCommand.ExecuteReaderAsync();
+            while (reader.Read())
+            {
+                string villainName = (string)reader["Name"];
+                int minionsCount = (int)reader["MinionsCount"];
+                sb.Append($"{villainName} - {minionsCount}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+
+}
