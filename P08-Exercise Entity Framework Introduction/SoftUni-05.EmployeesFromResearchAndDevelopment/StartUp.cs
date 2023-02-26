85ddd08015dbc768e8267ee627ac527812faa83b
iliyazz <iliyaz.softuni@gmail.com>
2023-02-26 17:51:02 +0200

    Create P05.EmployeesFromResearchAndDevelopment

---
+++ P08-Exercise Entity Framework Introduction/SoftUni-05.EmployeesFromResearchAndDevelopment/StartUp.cs
@@ -0,0 +1,45 @@
+﻿namespace SoftUni;
+
+using System.Text;
+
+using Data;
+using Microsoft.EntityFrameworkCore;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        SoftUniContext dbContext = new SoftUniContext();
+        string result = GetEmployeesFromResearchAndDevelopment(dbContext);
+        Console.WriteLine(result);
+    }
+
+    //Problem 05
+
+    public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        var employeesRnD = context.Employees
+            .Where(e => e.Department.Name == "Research and Development")
+            .Select(e => new
+            {
+                e.FirstName,
+                e.LastName,
+                DepartmentName = e.Department.Name,
+                e.Salary
+            })
+            .OrderBy(e => e.Salary)
+            .ThenByDescending(e => e.FirstName)
+            .ToArray();
+        foreach (var e in employeesRnD)
+        {
+            sb.AppendLine($"{e.FirstName} {e.LastName} from {e.DepartmentName} - ${e.Salary:f2}");
+        }
+        return sb.ToString().Trim();
+    }
+}
+
+
+
+
