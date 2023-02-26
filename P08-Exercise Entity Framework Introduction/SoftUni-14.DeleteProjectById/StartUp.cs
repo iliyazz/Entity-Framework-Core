f63d928acab42fd2530492999ff7bce5fb432df8
iliyazz <iliyaz.softuni@gmail.com>
2023-02-26 18:00:32 +0200

    Create P14.DeleteProjectById

---
+++ P08-Exercise Entity Framework Introduction/SoftUni-14.DeleteProjectById/StartUp.cs
@@ -0,0 +1,45 @@
+﻿namespace SoftUni;
+
+using System.Globalization;
+using System.Text;
+
+using Data;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        SoftUniContext dbContext = new SoftUniContext();
+        string result = DeleteProjectById(dbContext);
+        Console.WriteLine(result);
+    }
+
+    //Problem 14
+    public static string DeleteProjectById(SoftUniContext context)
+    {
+        var employeeProjectToDelete = context.EmployeesProjects
+            .Where(x => x.ProjectId == 2);
+        context.EmployeesProjects.RemoveRange(employeeProjectToDelete);
+
+        var projectToDelete = context.Projects
+            .Find(2);
+        if (projectToDelete != null)
+        {
+            context.Projects.Remove(projectToDelete);
+        }
+
+        context.SaveChanges();
+
+        var tenProjects = context.Projects
+            .Select(x => x.Name)
+            .Take(10)
+            .ToArray();
+        return string.Join(Environment.NewLine, tenProjects);
+    }
+}
+
+
+
+
