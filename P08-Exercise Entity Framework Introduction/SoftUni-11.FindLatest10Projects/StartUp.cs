0de2e20373078ca6c5b9c6e0ed897151b57ef3af
iliyazz <iliyaz.softuni@gmail.com>
2023-02-26 17:57:41 +0200

    Create P11.FindLatest10Projects

---
+++ P08-Exercise Entity Framework Introduction/SoftUni-11.FindLatest10Projects/StartUp.cs
@@ -0,0 +1,49 @@
+﻿namespace SoftUni;
+
+using System.Globalization;
+using System.Text;
+
+using Data;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        SoftUniContext dbContext = new SoftUniContext();
+        string result = GetLatestProjects(dbContext);
+        Console.WriteLine(result);
+    }
+
+    //Problem 11
+    public static string GetLatestProjects(SoftUniContext context)
+    {
+        var latestTenProjects = context.Projects
+            .OrderByDescending(x => x.StartDate)
+            .Take(10)
+            .Select(x => new
+            {
+                x.Name,
+                x.Description,
+                x.StartDate
+            })
+            .OrderBy(x => x.Name)
+            .ToArray();
+        StringBuilder sb = new StringBuilder();
+        foreach (var l in latestTenProjects)
+        {
+            sb.AppendLine(l.Name)
+                .AppendLine(l.Description)
+                .AppendLine(l.StartDate.ToString("M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture));
+        }
+
+        return sb.ToString().TrimEnd();
+
+    }
+
+}
+
+
+
+
