e76cb4f9321638918a890750ff64e0cc702e0a11
iliyazz <iliyaz.softuni@gmail.com>
2023-02-26 17:47:39 +0200

    Create P03.EmployeesFullInformation

---
+++ P08-Exercise Entity Framework Introduction/SoftUni-02.DatabaseFirst/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using SoftUni.Data;
+namespace SoftUni;
+
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        SoftUniContext dbContexto = new SoftUniContext();
+        Console.WriteLine("Connection Success");
+    }
+}
---
+++ P08-Exercise Entity Framework Introduction/SoftUni-03.EmployeesFullInformation/StartUp.cs
@@ -0,0 +1,44 @@
+﻿namespace SoftUni;
+
+using System.Text;
+
+using Data;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+
+
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        SoftUniContext dbContext = new SoftUniContext();
+        string result = GetEmployeesFullInformation(dbContext);
+        Console.WriteLine(result);
+    }
+
+    public static string GetEmployeesFullInformation(SoftUniContext context)
+    {
+        StringBuilder sb = new StringBuilder();
+        var employees = context.Employees
+            .OrderBy(e => e.EmployeeId)
+            .Select(e => new
+            {
+                e.FirstName,
+                e.LastName,
+                e.MiddleName,
+                e.JobTitle,
+                e.Salary/
+            })
+            .ToArray();
+
+        foreach (var e in employees)
+        {
+            sb
+                .AppendLine($"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:f2}");
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+}
