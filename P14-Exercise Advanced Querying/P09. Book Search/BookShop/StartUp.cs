848b678932e176213796f8b61c29de13e0d14e3b
iliyazz <iliyaz.softuni@gmail.com>
2023-03-04 15:03:02 +0200

    Create P09. Book Search

---
+++ P14-Exercise Advanced Querying/P09. Book Search/BookShop/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace BookShop;
+
+using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+using BookShop.Models;
+using Data;
+using Microsoft.EntityFrameworkCore;
+using Models.Enums;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        using var context = new BookShopContext();
+        //DbInitializer.ResetDatabase(db);
+        string input = Console.ReadLine();
+        Console.WriteLine(GetBookTitlesContaining(context, input));
+    }
+
+    //Problem 09
+    public static string GetBookTitlesContaining(BookShopContext context, string input)
+    {
+        var output = context
+            .Books
+            .Where(b => b.Title.ToLower().Contains(input.ToLower()))
+            .Select(b => b.Title)
+            .OrderBy(b => b)
+            .AsNoTracking()
+            .ToArray();
+        return string.Join(Environment.NewLine, output);
+    }
+
+}
