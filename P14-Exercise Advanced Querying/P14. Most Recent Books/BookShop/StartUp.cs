f2acc2f281faa6fb8730e4179c0c1ec4267cfa24
iliyazz <iliyaz.softuni@gmail.com>
2023-03-04 18:05:51 +0200

    Create P14. Most Recent Books

---
+++ P14-Exercise Advanced Querying/P14. Most Recent Books/BookShop/StartUp.cs
@@ -0,0 +1,45 @@
+﻿namespace BookShop;
+
+using System.Globalization;
+using System.Text;
+using Data;
+using Microsoft.EntityFrameworkCore;
+using Models.Enums;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        using var context = new BookShopContext();
+        //DbInitializer.ResetDatabase(db);
+        Console.WriteLine(GetMostRecentBooks(context));
+    }
+
+    //Problem 14
+    public static string GetMostRecentBooks(BookShopContext context)
+    {
+        var output = context
+            .Categories
+            .Select(c => new
+            {
+                Category = c.Name,
+                Books = c.CategoryBooks
+                    .Select(b => b.Book)
+                    .OrderByDescending(b => b.ReleaseDate)
+                    .Take(3)
+            })
+            .OrderBy(c => c.Category)
+            .ToList();
+
+        StringBuilder sb = new StringBuilder();
+        foreach (var info in output)
+        {
+            sb.AppendLine($"--{info.Category}");
+            foreach (var book in info.Books)
+            {
+                sb.AppendLine($"{book.Title} ({book.ReleaseDate.Value.Year})");
+            }
+        }
+        return sb.ToString().TrimEnd();
+    }
+}
