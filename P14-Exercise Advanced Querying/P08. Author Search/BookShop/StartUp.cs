f1796a0c089bd61873022426a51d8fee51d72940
iliyazz <iliyaz.softuni@gmail.com>
2023-03-04 14:52:29 +0200

    Create P08. Author Search

---
+++ P14-Exercise Advanced Querying/P08. Author Search/BookShop/StartUp.cs
@@ -0,0 +1,42 @@
+﻿namespace BookShop;
+
+using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+using BookShop.Models;
+using Data;
+using Microsoft.EntityFrameworkCore;
+using Models.Enums;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        using var context = new BookShopContext();
+        //DbInitializer.ResetDatabase(db);
+        string input = Console.ReadLine();
+        Console.WriteLine(GetAuthorNamesEndingIn(context, input));
+    }
+
+    //Problem 08
+    public static string GetAuthorNamesEndingIn(BookShopContext context, string input)
+    {
+        var output = context
+            .Authors
+            .Where(a => a.FirstName.EndsWith(input))
+            .OrderBy(a => a.FirstName)
+            .ThenBy(a => a.LastName)
+            .Select(a => new
+            {
+                FullName = $"{a.FirstName} {a.LastName}"
+            })
+            .AsNoTracking()
+            .ToArray();
+        StringBuilder sb = new StringBuilder();
+        foreach (var fn in output)
+        {
+            sb.AppendLine(fn.FullName);
+        }
+        return sb.ToString().TrimEnd();
+    }
+}
