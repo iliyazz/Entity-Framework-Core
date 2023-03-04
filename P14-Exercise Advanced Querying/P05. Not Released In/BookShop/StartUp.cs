79b62c3e37ae79a83977b3123cb1b6a05dc9b850
iliyazz <iliyaz.softuni@gmail.com>
2023-03-04 09:55:55 +0200

    Create P05. Not Released In

---
+++ P14-Exercise Advanced Querying/P05. Not Released In/BookShop/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace BookShop;
+
+using System.Globalization;
+using System.Text;
+using BookShop.Models;
+using Data;
+using Microsoft.EntityFrameworkCore;
+using Models.Enums;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        using var context = new BookShopContext();
+        //DbInitializer.ResetDatabase(db);
+        int input = int.Parse(Console.ReadLine());
+        Console.WriteLine(GetBooksNotReleasedIn(context, input));
+    }
+
+    //Problem 05
+    public static string GetBooksNotReleasedIn(BookShopContext context, int year)
+    {
+        var bookNotReleasedInGivenYear = context
+            .Books
+            .Where(b => b.ReleaseDate.Value.Year != year)
+            .OrderBy(b => b.BookId)
+            .Select(b => b.Title)
+            .AsNoTracking()
+            .ToArray();
+        return string.Join(Environment.NewLine, bookNotReleasedInGivenYear).TrimEnd();
+    }
+
+}
