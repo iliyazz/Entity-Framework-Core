24f7bf62766dc141b932886853c67c274c39baa5
iliyazz <iliyaz.softuni@gmail.com>
2023-03-04 15:29:59 +0200

    Create P10. Book Search by Author

---
+++ P14-Exercise Advanced Querying/P10. Book Search by Author/BookShop/StartUp.cs
@@ -0,0 +1,50 @@
+﻿namespace BookShop;
+
+using System.Globalization;
+using System.Text;
+using Data;
+using Microsoft.EntityFrameworkCore;
+using Models.Enums;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        using var context = new BookShopContext();
+        //DbInitializer.ResetDatabase(db);
+        string input = Console.ReadLine();
+        Console.WriteLine(GetBooksByAuthor(context, input));
+    }
+
+    //Problem 10
+    public static string GetBooksByAuthor(BookShopContext context, string input)
+    {
+        var output = context
+            .Authors
+            .Where(a => a.LastName.ToLower().StartsWith(input.ToLower()))
+            .Select(a => new
+            {
+                AuthorFirstName = a.FirstName,
+                AuthorLastName = a.LastName,
+                bookTitle = a.Books
+                    .Select(b => new
+                    {
+                        BookTitle = b.Title
+                    })
+
+            })
+            .AsNoTracking()
+            .ToArray();
+
+        StringBuilder sb = new StringBuilder();
+        foreach (var info in output)
+        {
+            foreach (var b in info.bookTitle)
+            {
+                sb.AppendLine($"{b.BookTitle} ({info.AuthorFirstName} {info.AuthorLastName})");
+            }
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+}
