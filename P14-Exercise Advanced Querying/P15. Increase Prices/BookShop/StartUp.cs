5f10e33fead213fa05a0ff353130164d1443c30e
iliyazz <iliyaz.softuni@gmail.com>
2023-03-04 18:46:31 +0200

    Create P15. Increase Prices

---
+++ P14-Exercise Advanced Querying/P15. Increase Prices/BookShop/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace BookShop;
+
+using System.Globalization;
+using System.Text;
+using Data;
+using Microsoft.EntityFrameworkCore;
+using Models.Enums;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        using var context = new BookShopContext();
+        //DbInitializer.ResetDatabase(db);
+        IncreasePrices(context);
+    }
+
+    //Problem 15
+    public static void IncreasePrices(BookShopContext context)
+    {
+        var output = context
+            .Books
+            .Where(b => b.ReleaseDate.Value.Year < 2010);
+
+        foreach (var book in output)
+        {
+            book.Price += 5;
+        }
+
+        context.SaveChanges();
+    }
+}
