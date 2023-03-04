a76f5b648789d9e3cd067b9fe22a3de79a4e29cb
iliyazz <iliyaz.softuni@gmail.com>
2023-03-04 09:55:15 +0200

    Create P03. Golden Books

---
+++ P14-Exercise Advanced Querying/P03. Golden Books/BookShop/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace BookShop;
+
+using System.Text;
+using Data;
+using Initializer;
+using Microsoft.EntityFrameworkCore;
+using Models.Enums;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        using var context = new BookShopContext();
+        //DbInitializer.ResetDatabase(db);
+
+        Console.WriteLine(GetGoldenBooks(context));
+    }
+    //Problem 03
+    public static string GetGoldenBooks(BookShopContext context)
+    {
+        string[] bookTitles = context
+            .Books
+            .Where(b => b.EditionType == EditionType.Gold && b.Copies < 5000)
+            .OrderBy(b => b.BookId)
+            .Select(b => b.Title)
+            .AsNoTracking()
+            .ToArray();
+        return string.Join(Environment.NewLine, bookTitles);
+    }
+}
