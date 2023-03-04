57bac29cbbfb85abfc72183cd08633973fbb7431
iliyazz <iliyaz.softuni@gmail.com>
2023-03-04 17:04:13 +0200

    Create P12. Total Book Copies

---
+++ P14-Exercise Advanced Querying/P12. Total Book Copies/BookShop/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace BookShop;
+
+using System.Globalization;
+using System.Text;
+using Data;
+using Microsoft.EntityFrameworkCore;
+using Models.Enums;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        using var context = new BookShopContext();
+        //DbInitializer.ResetDatabase(db);
+        //int input = int.Parse(Console.ReadLine());
+        Console.WriteLine(CountCopiesByAuthor(context));
+    }
+
+    //Problem 12
+    public static string CountCopiesByAuthor(BookShopContext context)
+    {
+        var output = context
+            .Authors
+            .Select(a => new
+            {
+                AuthorName = $"{a.FirstName} {a.LastName}",
+                BooksCopies = a.Books
+                    .Sum(b => b.Copies)
+            })
+            .OrderByDescending(b => b.BooksCopies)
+            .AsNoTracking()
+            .ToArray();
+         
+        StringBuilder sb = new StringBuilder();
+        foreach (var item in output)
+
+            sb.AppendLine($"{item.AuthorName} - {item.BooksCopies}");
+        return sb.ToString().TrimEnd();
+    }
+
+}
