4243ac8e59af6e8ccc60370b65abfb6be3491d78
iliyazz <iliyaz.softuni@gmail.com>
2023-03-04 19:27:59 +0200

    Create P16. Remove Books

---
+++ P14-Exercise Advanced Querying/P16. Remove Books/BookShop/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace BookShop;
+
+using System.Globalization;
+using System.Text;
+using Data;
+using Microsoft.EntityFrameworkCore;
+using Models.Enums;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        using var context = new BookShopContext();
+        //DbInitializer.ResetDatabase(db);
+        Console.WriteLine(RemoveBooks(context));
+    }
+
+    //Problem 16
+    public static int RemoveBooks(BookShopContext context)
+    {
+        var output = context
+            .Books
+            .Where(b => b.Copies < 4200);
+        context.Books.RemoveRange(output);
+        int countOfRemovedBooks = output.Count();
+        context.SaveChanges();
+        return countOfRemovedBooks;
+    }
+}
