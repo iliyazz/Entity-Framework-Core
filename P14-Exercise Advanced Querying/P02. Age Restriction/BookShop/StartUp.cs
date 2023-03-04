a94c3fda61a43eb6155881c3973bbeff6ec2a7bd
iliyazz <iliyaz.softuni@gmail.com>
2023-03-04 09:54:52 +0200

    Create P02. Age Restriction

---
+++ P14-Exercise Advanced Querying/P02. Age Restriction/BookShop/StartUp.cs
@@ -0,0 +1,39 @@
+﻿namespace BookShop;
+
+using System.Text;
+using Data;
+using Initializer;
+using Microsoft.EntityFrameworkCore;
+using Models.Enums;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        using var db = new BookShopContext();
+        //DbInitializer.ResetDatabase(db);
+        string input = Console.ReadLine();
+
+        Console.WriteLine(GetBooksByAgeRestriction(db, input));
+    }
+
+    //Problem 02
+    public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+    {
+        //StringBuilder sb = new StringBuilder();
+        AgeRestriction ageRestriction;
+            bool isParsed= Enum.TryParse<AgeRestriction>(command, true, out ageRestriction);
+            if (!isParsed)
+            {
+                return String.Empty;
+            }
+        string[] bookTitles = context
+            .Books
+            .Where(b => b.AgeRestriction == ageRestriction)
+            .Select(b => b.Title)
+            .OrderBy(t => t)
+            .AsNoTracking()
+            .ToArray();
+        return string.Join(Environment.NewLine, bookTitles);
+    }
+}
