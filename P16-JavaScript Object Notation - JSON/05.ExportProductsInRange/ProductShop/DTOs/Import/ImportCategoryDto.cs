89348123a76ab2c49838fdf89fb1662be0cff9fa
iliyazz <iliyaz.softuni@gmail.com>
2023-03-14 23:30:03 +0200

    Create 05. Export Products In Range

---
+++ P16-JavaScript Object Notation - JSON/05.ExportProductsInRange/ProductShop/DTOs/Import/ImportCategoryDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductShop.DTOs.Import
+{
+    using Microsoft.Extensions.Primitives;
+    using Newtonsoft.Json;
+
+    public class ImportCategoryDto
+    {
+        [JsonProperty("name")]
+        public string? Name { get; set; }
+    }
+}
---
+++ P16-JavaScript Object Notation - JSON/05.ExportProductsInRange/ProductShop/DTOs/Import/ImportProductDto.cs
@@ -0,0 +1,15 @@
+﻿namespace ProductShop.DTOs.Import
+{
+
+
+    public class ImportProductDto
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int SellerId { get; set; }
+        public int? BuyerId { get; set; }
+
+
+
+    }
+}
