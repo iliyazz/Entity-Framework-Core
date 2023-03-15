f5afffe83ce4f9937be4cbfa6a481096a8a1206c
iliyazz <iliyaz.softuni@gmail.com>
2023-03-15 23:52:49 +0200

    Create 10. Import Parts

---
+++ P16-JavaScript Object Notation - JSON/10.ImportParts/CarDealer/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace CarDealer.Data
+{
+    public static class Configuration
+    {
+        public const string ConnectionString = @"Server=DESKTOP-S4MI5RU\SQLEXPRESS;Database=CarDealer;Integrated Security=True;Encrypt=False";
+    }
+}
