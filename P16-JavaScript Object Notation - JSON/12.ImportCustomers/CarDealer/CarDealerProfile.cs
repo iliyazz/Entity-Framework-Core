4bfad9e255717e5379930fb54c24ea6a2f8adb6f
iliyazz <iliyaz.softuni@gmail.com>
2023-03-17 20:59:26 +0200

    Create 12. Import Customers

---
+++ P16-JavaScript Object Notation - JSON/12.ImportCustomers/CarDealer/CarDealerProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using CarDealer.Data;
+using Newtonsoft.Json.Serialization;
+
+namespace CarDealer
+{
+    using DTOs.Import;
+    using Models;
+
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            //Supplier
+            this.CreateMap<ImportSupplierDto, Supplier>();
+
+            //Part
+            this.CreateMap<ImportPartDto, Part>();
+
+            //Car
+            this.CreateMap<ImportCarDto, Car>();
+
+            //Customer
+            this.CreateMap<ImportCustomerDto, Customer>();
+
+        }
+    }
+}
