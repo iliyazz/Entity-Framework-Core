0c4c3ebd8a4c91d9bbf70a0f49b14aaa8f94e866
iliyazz <iliyaz.softuni@gmail.com>
2023-03-18 10:00:38 +0200

    Create 17. Export Cars With Their List Of Parts

---
+++ P16-JavaScript Object Notation - JSON/17.ExportCarsWithTheirListOfParts/CarDealer/DTOs/Import/ImportPartDto.cs
@@ -0,0 +1,34 @@
+﻿namespace CarDealer.DTOs.Import
+{
+    using Newtonsoft.Json;
+    using System.Xml.Linq;
+
+    //[JsonObject]
+    public class ImportPartDto
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; } = null!;
+
+        [JsonProperty("price")]
+        public decimal Price { get; set; }
+
+        [JsonProperty("quantity")]
+        public int Quantity { get; set; }
+
+        [JsonProperty("supplierId")]
+        public int SupplierId { get; set; }
+
+        /*
+        "name": "Bonnet/hood",
+        "price": 1001.34,
+        "quantity": 10,
+        "supplierId": 17
+        -----------------------------
+        public int Id { get; set; }
+        public string Name { get; set; } = null!; 
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public int SupplierId { get; set; }
+         */
+    }
+}
