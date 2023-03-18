c1e27b0c2f589c1cbcdd3637886ba6c785c815cd
iliyazz <iliyaz.softuni@gmail.com>
2023-03-18 17:21:34 +0200

    Create 02. Import Products

---
+++ P20-Extensible Markup Language - XML/02.ImportProducts/ProductShop/ProductShopProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+
+namespace ProductShop
+{
+    using DTOs.Import;
+    using Models;
+
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            //User
+            this.CreateMap<ImportUserDto, User>();
+
+            //Product
+            this.CreateMap<ImportProductDto, Product>();
+        }
+    }
+}
