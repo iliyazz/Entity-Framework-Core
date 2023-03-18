a2c327756fefdfdda792ce4caf5dab33cc3a598e
iliyazz <iliyaz.softuni@gmail.com>
2023-03-18 18:15:16 +0200

    Create 04. Import Categories and Products

---
+++ P20-Extensible Markup Language - XML/04.ImportCategoriesAndProducts/ProductShop/ProductShopProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+
+namespace ProductShop
+{
+    using DTOs.Import;
+    using Models;
+
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            //User
+            this.CreateMap<ImportUserDto, User>();
+
+            //Product
+            this.CreateMap<ImportProductDto, Product>();
+
+            //categories
+            this.CreateMap<ImportCategoryDto, Category>();
+
+            //CategoryProduct
+            this.CreateMap<ImportCategoryProductDto, CategoryProduct>();
+        }
+    }
+}
