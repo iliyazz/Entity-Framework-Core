03ab3f380b57b4daf11d3fb822eb2ff35adf6e32
iliyazz <iliyaz.softuni@gmail.com>
2023-03-18 10:29:22 +0200

    Create 18. Export Total Sales By Customer

---
+++ P16-JavaScript Object Notation - JSON/18.ExportTotalSalesByCustomer/CarDealer/DTOs/Import/ImportCarDto.cs
@@ -0,0 +1,50 @@
+﻿namespace CarDealer.DTOs.Import
+{
+    using Newtonsoft.Json;
+
+    public class ImportCarDto
+    {
+        [JsonProperty("make")]
+        public string Make { get; set; } = null!;
+
+        [JsonProperty("model")]
+        public string Model { get; set; } = null!;
+
+        [JsonProperty("traveledDistance")]
+        public long TraveledDistance { get; set; }
+
+        [JsonProperty("partsId")]
+        public int[] PartsId { get; set; }
+
+
+
+
+
+        /*
+        public int Id { get; set; }
+        public string Make { get; set; } = null!;
+        public string Model { get; set; } = null!;
+        public long TravelledDistance { get; set; }
+        public virtual ICollection<Sale> Sales { get; set; } = new List<Sale>();    
+        public virtual ICollection<PartCar> PartsCars { get; set; } = new List<PartCar>();
+
+
+         "make": "Opel",
+         "model": "Omega",
+         "traveledDistance": 176664996,
+         "partsId": [
+           38,
+           102,
+           23,
+           116,
+           46,
+           68,
+           88,
+           104,
+           71,
+           32,
+           114
+         ]
+         */
+    }
+}
---
+++ P16-JavaScript Object Notation - JSON/18.ExportTotalSalesByCustomer/CarDealer/DTOs/Import/ImportSaleDto.cs
@@ -0,0 +1,25 @@
+﻿namespace CarDealer.DTOs.Import
+{
+    using Newtonsoft.Json;
+
+    public class ImportSaleDto
+    {
+        [JsonProperty("discount")]
+        public decimal Discount { get; set; }
+
+
+        [JsonProperty("carId")]
+        public int CarId { get; set; }
+
+
+        [JsonProperty("customerId")]
+        public int CustomerId { get; set; }
+    }
+    /*
+       {
+         "carId": 105,
+         "customerId": 30,
+         "discount": 30
+        }
+     */
+}
