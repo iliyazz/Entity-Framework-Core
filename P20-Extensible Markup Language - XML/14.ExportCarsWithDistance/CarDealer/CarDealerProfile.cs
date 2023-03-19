02bd5024e969be9971cb5ec36726025493fe97a2
iliyazz <iliyaz.softuni@gmail.com>
2023-03-19 18:34:44 +0200

    Create 14. Export Cars With Distance

---
+++ P20-Extensible Markup Language - XML/14.ExportCarsWithDistance/CarDealer/CarDealerProfile.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+
+namespace CarDealer
+{
+    using System.Globalization;
+    using DTOs.Export;
+    using DTOs.Import;
+    using DTOs.Import.ImportCar;
+    using Models;
+
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            //Supplier
+            this.CreateMap<ImportSupplierDto, Supplier>();
+
+            //Part
+            this.CreateMap<ImportPartDto, Part>();
+
+            //Car
+            this.CreateMap<ImportCarDto, Car>()
+                .ForSourceMember(s => s.Parts, opt => opt.DoNotValidate());
+            this.CreateMap<Car, ExportCarsWithDistanceDto>();
+
+            //Customer
+            this.CreateMap<ImportCustomerDto, Customer>()
+                .ForMember(dest => dest.BirthDate, opt => opt.MapFrom(src => DateTime.Parse(src.BirthDate, CultureInfo.InvariantCulture)));
+
+            //Sale
+            this.CreateMap<ImportSaleDto, Sale>();
+
+        }
+    }
+}
---
+++ P20-Extensible Markup Language - XML/14.ExportCarsWithDistance/CarDealer/DTOs/Import/ImportCustomerDto.cs
@@ -0,0 +1,26 @@
+﻿namespace CarDealer.DTOs.Import
+{
+    using System.Xml.Serialization;
+
+    [XmlType("Customer")]
+    public class ImportCustomerDto
+    {
+        [XmlElement("name")]
+        public string Name { get; set; } = null!;
+
+
+        [XmlElement("birthDate")] public string BirthDate { get; set; } = null!;
+
+
+        [XmlElement("isYoungDriver")]
+        public bool IsYoungDriver { get; set; }
+    }
+}
+/*
+ <Customers>
+    <Customer>
+        <name>Emmitt Benally</name>
+        <birthDate>1993-11-20T00:00:00</birthDate>
+        <isYoungDriver>true</isYoungDriver>
+    </Customer>
+ */
