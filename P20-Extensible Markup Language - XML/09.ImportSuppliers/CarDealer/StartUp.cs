3e7828ab1cc69c37ff12844722e423733657d141
iliyazz <iliyaz.softuni@gmail.com>
2023-03-19 14:11:47 +0200

    Create 09. Import Suppliers

---
+++ P20-Extensible Markup Language - XML/09.ImportSuppliers/CarDealer/StartUp.cs
@@ -0,0 +1,50 @@
+﻿namespace CarDealer
+{
+    using AutoMapper;
+    using Data;
+    using DTOs.Import;
+    using Models;
+    using Utilities;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+
+            //01
+            string inputXml = File.ReadAllText("../../../Datasets/suppliers.xml");
+            string output = ImportSuppliers(context, inputXml);
+            Console.WriteLine(output);
+        }
+
+
+        //01
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            IMapper mapper = InitializeAutoMapper();
+            XmlHelper xmlHelper = new XmlHelper();
+            ImportSupplierDto[] supplierDtos = xmlHelper.Deserialize<ImportSupplierDto[]>(inputXml, "Suppliers");
+            ICollection<Supplier> suppliers = new HashSet<Supplier>();
+            foreach (var supplierDto in supplierDtos)
+            {
+                if (string.IsNullOrEmpty(supplierDto.Name))
+                {
+                    continue;
+                }
+
+                Supplier supplier = mapper.Map<Supplier>(supplierDto);
+                suppliers.Add(supplier);
+            }
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+            return $"Successfully imported {suppliers.Count}";
+        }
+
+
+        private static IMapper InitializeAutoMapper() => new Mapper(new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<CarDealerProfile>();
+        }));
+    }
+}
