3e5fff53e9abfe1cafa8ec0509dd680c6feabc23
iliyazz <iliyaz.softuni@gmail.com>
2023-03-19 09:23:18 +0200

    Create 06. Export Sold Products

---
+++ P20-Extensible Markup Language - XML/06.ExportSoldProducts/ProductShop/ProductShopProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+
+namespace ProductShop
+{
+    using DTOs.Export;
+    using DTOs.Import;
+    using Models;
+
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            //User
+            this.CreateMap<ImportUserDto, User>();
+            this.CreateMap<User, ExportSoldProductDto>()
+                .ForMember(dest => dest.SoldProducts, opt => opt.MapFrom(s => s.ProductsSold));
+
+            //Product
+            this.CreateMap<ImportProductDto, Product>();
+            this.CreateMap<Product, ExportProductsInRangeDto>()
+                .ForMember(dest => dest.Fullname, opt => opt.MapFrom(src => $"{src.Buyer.FirstName} {src.Buyer.LastName}"));
+            this.CreateMap<Product, ExportProductUserDto>();
+
+
+            //categories
+            this.CreateMap<ImportCategoryDto, Category>();
+
+            //CategoryProduct
+            this.CreateMap<ImportCategoryProductDto, CategoryProduct>();
+        }
+    }
+}
