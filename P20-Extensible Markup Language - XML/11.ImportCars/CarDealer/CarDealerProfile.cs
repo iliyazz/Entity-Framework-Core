c045098a22d63f7e10a66d55268da7722adcbf4e
iliyazz <iliyaz.softuni@gmail.com>
2023-03-19 15:17:47 +0200

    Create 11. Import Cars

---
+++ P20-Extensible Markup Language - XML/11.ImportCars/CarDealer/CarDealerProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+
+namespace CarDealer
+{
+    using DTOs.Import;
+    using DTOs.Import.NewFolder;
+    using Models;
+
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            //Supplier
+            this.CreateMap<ImportSupplierDto, Supplier>();
+
+            //Part
+            this.CreateMap<ImportPartDto, Part>();
+
+            //Car
+            this.CreateMap<ImportCarDto, Car>()
+                .ForSourceMember(s => s.Parts, opt => opt.DoNotValidate());
+
+        }
+    }
+}
