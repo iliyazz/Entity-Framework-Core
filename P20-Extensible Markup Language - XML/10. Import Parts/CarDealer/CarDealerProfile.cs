381e88ce4dc9f864b26d33558e009633b5ff6ffd
iliyazz <iliyaz.softuni@gmail.com>
2023-03-19 14:38:08 +0200

    Create 10. Import Parts

---
+++ P20-Extensible Markup Language - XML/10. Import Parts/CarDealer/CarDealerProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+
+namespace CarDealer
+{
+    using DTOs.Import;
+    using Models;
+
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            //Supplier
+            this.CreateMap<ImportSupplierDto, Supplier>();
+
+            //Part
+            this.CreateMap<ImportPartDto, Part>();
+        }
+    }
+}
---
+++ P20-Extensible Markup Language - XML/10. Import Parts/CarDealer/StartUp.cs
@@ -0,0 +1,83 @@
+﻿namespace CarDealer
+{
+    using AutoMapper;
+    using Data;
+    using DTOs.Import;
+    using Models;
+    using Utilities;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+
+            //09
+            //string inputXml = File.ReadAllText("../../../Datasets/suppliers.xml");
+            //string output = ImportSuppliers(context, inputXml);
+
+            //10
+            string inputXml = File.ReadAllText("../../../Datasets/parts.xml");
+            string output = ImportParts(context, inputXml);
+
+
+
+
+
+            Console.WriteLine(output);
+        }
+        //10
+        public static string ImportParts(CarDealerContext context, string inputXml)
+        {
+            IMapper mapper = InitializeAutoMapper();
+            XmlHelper xmlHelper = new XmlHelper();
+            ImportPartDto[] partDtos = xmlHelper.Deserialize<ImportPartDto[]>(inputXml, "Parts");
+            ICollection<Part> parts = new HashSet<Part>();
+            foreach (var partDto in partDtos)
+            {
+                if (string.IsNullOrEmpty(partDto.Name))
+                {
+                    continue;
+                }
+
+                if (!partDto.SupplierId.HasValue || !context.Suppliers.Any(s => s.Id == partDto.SupplierId))
+                {
+                    continue;
+                }
+                Part part = mapper.Map<Part>(partDto);
+                parts.Add(part);
+            }
+            context.Parts.AddRange(parts);
+            context.SaveChanges();
+            return $"Successfully imported {parts.Count}";
+        }
+
+        //09
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            IMapper mapper = InitializeAutoMapper();
+            XmlHelper xmlHelper = new XmlHelper();
+            ImportSupplierDto[] supplierDtos = xmlHelper.Deserialize<ImportSupplierDto[]>(inputXml, "Suppliers");
+            ICollection<Supplier> suppliers = new HashSet<Supplier>();
+            foreach (var supplierDto in supplierDtos)
+            {
+                if (string.IsNullOrEmpty(supplierDto.Name))
+                {
+                    continue;
+                }
+
+                Supplier supplier = mapper.Map<Supplier>(supplierDto);
+                suppliers.Add(supplier);
+            }
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+            return $"Successfully imported {suppliers.Count}";
+        }
+
+
+        private static IMapper InitializeAutoMapper() => new Mapper(new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<CarDealerProfile>();
+        }));
+    }
+}
