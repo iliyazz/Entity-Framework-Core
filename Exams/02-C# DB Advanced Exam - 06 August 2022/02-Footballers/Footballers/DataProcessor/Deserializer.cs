068debdd6bd28c06383be392ef75ef7068b508c6
iliyazz <iliyaz.softuni@gmail.com>
2023-03-25 11:36:42 +0200

    Create 02. Data Import

---
+++ Exams/02-C# DB Advanced Exam - 06 August 2022/02-Footballers/Footballers/DataProcessor/Deserializer.cs
@@ -0,0 +1,166 @@
+﻿namespace Footballers.DataProcessor
+{
+    using Footballers.Data;
+    using System.ComponentModel.DataAnnotations;
+    using System.Globalization;
+    using System.Text;
+    using System.Xml;
+    using System.Xml.Serialization;
+    using Data.Models;
+    using Data.Models.Enums;
+    using ImportDto;
+    using Newtonsoft.Json;
+    using System.Reflection.PortableExecutable;
+
+    public class Deserializer
+    {
+        private const string ErrorMessage = "Invalid data!";
+
+        private const string SuccessfullyImportedCoach
+            = "Successfully imported coach - {0} with {1} footballers.";
+
+        private const string SuccessfullyImportedTeam
+            = "Successfully imported team - {0} with {1} footballers.";
+
+        public static string ImportCoaches(FootballersContext context, string xmlString)
+        {
+            StringBuilder sb = new StringBuilder();
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ImportCoachDto[]), new XmlRootAttribute("Coaches"));
+            using StringReader reader = new StringReader(xmlString);
+            ImportCoachDto[] coachDtos = (ImportCoachDto[])xmlSerializer.Deserialize(reader);
+            List<Coach> coaches = new List<Coach>();
+
+            foreach (var coachDto in coachDtos)
+            {
+                if (!IsValid(coachDto))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                string nationality = coachDto.Nationality;
+                if (string.IsNullOrEmpty(nationality))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                Coach coach = new Coach()
+                {
+                    Name = coachDto.Name,
+                    Nationality = nationality,
+                };
+
+
+
+                foreach (var footballerDto in coachDto.Footballers)
+                {
+                    if (!IsValid(footballerDto))
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    DateTime footballerContractStartDate;
+                    DateTime footballerContractEndDate;
+                    bool isFootballerContractStartDateValid = DateTime.TryParseExact(footballerDto.ContractStartDate, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out footballerContractStartDate);
+                    bool isFootballerContractEndDateValid = DateTime.TryParseExact(footballerDto.ContractEndDate, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out footballerContractEndDate);
+                    if (!isFootballerContractStartDateValid || !isFootballerContractEndDateValid)
+                    {
+                        if (IsValid(footballerDto))
+                        {
+                            sb.AppendLine(ErrorMessage);
+                            continue;
+                        }
+                    }
+
+                    if (footballerContractEndDate <= footballerContractStartDate)
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+
+                    Footballer footballer = new Footballer()
+                    {
+                        Name = footballerDto.Name,
+                        ContractStartDate = footballerContractStartDate,
+                        ContractEndDate = footballerContractEndDate,
+                        BestSkillType = (BestSkillType)footballerDto.BestSkillType,
+                        PositionType = (PositionType)footballerDto.PositionType,
+                    };
+                    coach.Footballers.Add(footballer);
+                }
+                coaches.Add(coach);
+                sb.AppendLine(string.Format(SuccessfullyImportedCoach, coach.Name, coach.Footballers.Count));
+            }
+            context.Coaches.AddRange(coaches);
+            context.SaveChanges();
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string ImportTeams(FootballersContext context, string jsonString)
+        {
+            var teamDtos = JsonConvert.DeserializeObject<ImportTeamDto[]>(jsonString);
+            StringBuilder sb = new StringBuilder();
+            var teams = new List<Team>();
+            foreach (var teamDto in teamDtos)
+            {
+                if (!IsValid(teamDto))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+                Team team = new Team()
+                {
+                    Name = teamDto.Name,
+                    Nationality = teamDto.Nationality,
+                    Trophies = teamDto.Trophies,
+                };
+                if (team.Trophies == 0)
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+
+
+
+
+                foreach (var footballer in teamDto.Footballers.Distinct())
+                {
+                    Footballer footballerId = context
+                        .Footballers
+                        .Find(footballer);
+                    if (footballerId == null)
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    TeamFootballer teamFootballer = new TeamFootballer()
+                    {
+                        Footballer = footballerId
+                    };
+                    team.TeamsFootballers.Add(teamFootballer);
+
+                }
+                teams.Add(team);
+                sb.AppendLine(string.Format(SuccessfullyImportedTeam, team.Name, team.TeamsFootballers.Count));
+
+            }
+            context.Teams.AddRange(teams);
+            context.SaveChanges();
+            return sb.ToString().TrimEnd();
+        }
+
+        private static bool IsValid(object dto)
+        {
+            var validationContext = new ValidationContext(dto);
+            var validationResult = new List<ValidationResult>();
+
+            return Validator.TryValidateObject(dto, validationContext, validationResult, true);
+        }
+    }
+}
---
+++ Exams/02-C# DB Advanced Exam - 06 August 2022/02-Footballers/Footballers/DataProcessor/ImportDto/ImportCoachDto.cs
@@ -0,0 +1,24 @@
+﻿namespace Footballers.DataProcessor.ImportDto
+{
+    using Footballers.Data.Models;
+    using System.ComponentModel.DataAnnotations;
+    using System.Xml.Serialization;
+
+    [XmlType("Coach")]
+    public class ImportCoachDto
+    {
+
+        [Required]
+        [MaxLength(40)]
+        [MinLength(2)]
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+
+        [XmlElement("Nationality")]
+        public string Nationality { get; set; }
+
+        [XmlArray("Footballers")]
+        public virtual ImportFootballerDto[] Footballers { get; set; }
+    }
+}
