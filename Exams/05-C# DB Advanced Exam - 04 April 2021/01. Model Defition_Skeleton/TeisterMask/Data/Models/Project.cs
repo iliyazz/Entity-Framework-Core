9bc0767a86a4b79f7e9d4c52da324d1a98df1660
iliyazz <iliyaz.softuni@gmail.com>
2023-03-26 23:20:34 +0300

    Create 02. Data Import

---
+++ Exams/05-C# DB Advanced Exam - 04 April 2021/01. Model Defition_Skeleton/TeisterMask/Data/Models/Project.cs
@@ -0,0 +1,37 @@
+﻿namespace TeisterMask.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using Common;
+    using TeisterMask.Data.Models.Enums;
+
+    public class Project
+    {
+        public Project()
+        {
+            this.Tasks = new HashSet<Task>();
+        }
+
+        [Key] public int Id { get; set; }
+
+
+        [Required]
+        [MinLength(ValidationConstants.TaskNameMinLength)]
+        [MaxLength(ValidationConstants.TaskNameMaxLength)]
+        public string Name { get; set; }
+
+
+        [Required]
+        public DateTime OpenDate { get; set; }
+
+
+        public DateTime? DueDate { get; set; }
+
+
+
+        public virtual ICollection<Task> Tasks { get; set; }
+
+
+
+    }
+}
---
+++ Exams/05-C# DB Advanced Exam - 04 April 2021/01. Model Defition_Skeleton/TeisterMask/DataProcessor/ImportDto/ImportPrTaskDto.cs
@@ -0,0 +1,39 @@
+﻿namespace TeisterMask.DataProcessor.ImportDto
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Xml.Serialization;
+    using TeisterMask.Common;
+    using TeisterMask.Data.Models.Enums;
+
+    [XmlType("Task")]
+    public class ImportPrTaskDto
+    {
+        [Required]
+        [MinLength(ValidationConstants.TaskNameMinLength)]
+        [MaxLength(ValidationConstants.TaskNameMaxLength)]
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+
+        [Required]
+        [XmlElement("OpenDate")]
+        public string OpenDate { get; set; }
+
+
+        [Required]
+        [XmlElement("DueDate")]
+        public string DueDate { get; set; }
+
+
+        [Required]
+        [XmlElement("ExecutionType")]
+        [Range(0,3)]
+        public int ExecutionType { get; set; }
+
+
+        [Required]
+        [XmlElement("LabelType")]
+        [Range(0, 4)]
+        public int LabelType { get; set; }
+    }
+}
