d79ab165297025a2792dead1f970592b523ea3fe
iliyazz <iliyaz.softuni@gmail.com>
2023-03-26 23:41:55 +0300

    Delete files

--- Exams/05-C# DB Advanced Exam - 04 April 2021/01. Model Defition_Skeleton/TeisterMask/Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TeisterMask.Data
-{
-    public static class Configuration
-    {
-        public static string ConnectionString = @"Server=DESKTOP-S4MI5RU\SQLEXPRESS;Database=TeisterMaskExam;Integrated Security=True;Encrypt=False";
-    }
-}
